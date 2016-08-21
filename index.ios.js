e3b9c975ac0a82b3bf4a0bbb1bf135a328b9b6a4
Chinwei Wong <chinweiwong@Chinweis-MacBook-Pro.local>
2016-08-21 10:17:10 -0700

    can add hard coded items to scroll list now

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,10 @@ import {
   Text,
   View,
   NavigatorIOS,
+  Animated,
+  Easing,
+  ScrollView,
+  ListView,
   TouchableHighlight
 } from 'react-native';
 
@@ -17,8 +21,8 @@ class splitBill extends Component{
       <NavigatorIOS
         style={{flex: 1, backgroundColor: '#f3ffff'}}
         initialRoute={{
-          title: 'first',
-          component: Home
+          title: 'Add Items',
+          component: AddItems
         }} />
     );
   }
@@ -29,11 +33,17 @@ class Home extends Component {
     super(props);
   }
 
+
+
+
   onPress() {
     console.log(this);
     this.props.navigator.push({
       title: 'Add Items',
-      component: AddItems
+      component: AddItems,
+      rightButtonTitle: 'Add',
+      onRightButtonPress: () => this._handleAdd(),
+
     })
   }
 
@@ -53,13 +63,97 @@ class Home extends Component {
 }
 
 class AddItems extends Component {
+
+
+
   constructor(props) {
     super(props);
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    console.log(ds);
+
+
+    this.state = {
+      receiptValue: '',
+      newData: ['new array!', 'another entry', 'sdfdf'],
+      dataSource: ds.cloneWithRows(['1','dfs'])
+    }
+  }
+
+
+
+
+  _handleAdd() {
+
+    // console.log(this.state.dataSource);
+    // this.state.dataSource.concat(['lala']);
+    console.log(this.state.dataSource);
+
+
+
+    this.setState({
+      newData: this.state.newData.concat(['lalala']),
+      dataSource: this.state.dataSource.cloneWithRows(this.state.newData),
+
+    })
+  }
+
+  appendNumber(value) {
+    if (this.state.receiptValue.length >= 6) {
+      value = ''
+    }
+
+    this.setState({
+      receiptValue: this.state.receiptValue + value
+    })
+  }
+
+  removeLastNumber() {
+    this.setState({
+      receiptValue: this.state.receiptValue.slice(0, -1)
+    })
   }
 
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: '#0cf'}}></View>
+      <View style={{flex: 1, backgroundColor: '#0cf', paddingTop: 64}}>
+        <View style={{height: 80, backgroundColor: '#494D4D', paddingBottom: 5, paddingRight: 5, paddingLeft: 5, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+            <Text style={{fontSize: 12, color: '#7C8282'}}>Total amount</Text>
+            <Text style={{fontSize: 24, color: '#F3FFFF'}}>{String(this.state.receiptValue)}</Text>
+        </View>
+
+        <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+        <TouchableHighlight onPress={() => this._handleAdd()}>
+            <Text>Click me!!!</Text>
+        </TouchableHighlight>
+
+        <View style={{flex: 1}}>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                  <NumPadKey text="1" appendNumber={() => this.appendNumber('1')}/>
+                  <NumPadKey text="2" appendNumber={() => this.appendNumber('2')}/>
+                  <NumPadKey text="3" appendNumber={() => this.appendNumber('3')}/>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                  <NumPadKey text="4" appendNumber={() => this.appendNumber('4')}/>
+                  <NumPadKey text="5" appendNumber={() => this.appendNumber('5')}/>
+                  <NumPadKey text="6" appendNumber={() => this.appendNumber('6')}/>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                  <NumPadKey text="7" appendNumber={() => this.appendNumber('7')}/>
+                  <NumPadKey text="8" appendNumber={() => this.appendNumber('8')}/>
+                  <NumPadKey text="9" appendNumber={() => this.appendNumber('9')}/>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                  <NumPadKey text="." appendNumber={() => this.appendNumber('.')}/>
+                  <NumPadKey text="0" appendNumber={() => this.appendNumber('0')}/>
+                  <NumPadKey text="Backsp" appendNumber={() => this.removeLastNumber()}/>
+              </View>
+            </View>
+
+      </View>
     )
   }
 }
@@ -109,6 +203,10 @@ class Tab extends Component {
           <Text>6</Text>
         </View>
       </TouchableHighlight>
+
+
+
+
     </View>
     )
   }
@@ -133,6 +231,79 @@ class TabItem extends Component {
     )
   }
 }
+
+class NumPadKey extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fontSize: 14,
+      fontPos: new Animated.Value(0),
+      fontSizeAnim: new Animated.Value(14)
+    }
+
+  }
+
+onPress() {
+  this.props.appendNumber();
+}
+
+onPressIn() {
+
+  Animated.timing(          // Uses easing functions
+     this.state.fontSizeAnim,    // The value to drive
+     {
+        toValue: 28,
+        duration: 200,
+        easing: Easing.elastic(1)
+   }).start();
+
+   Animated.timing(          // Uses easing functions
+      this.state.fontPos,    // The value to drive
+      {
+         toValue: 20,
+         duration: 200,
+         easing: Easing.elastic(1)
+    }).start();
+   }
+
+onPressOut() {
+  Animated.timing(          // Uses easing functions
+     this.state.fontSizeAnim,    // The value to drive
+     {toValue: 14,
+       duration: 200,
+       easing: Easing.elastic(2)
+     }            // Configuration
+   ).start();
+
+   Animated.timing(          // Uses easing functions
+      this.state.fontPos,    // The value to drive
+      {
+         toValue: 0,
+         duration: 200,
+         easing: Easing.elastic(2)
+    }).start();
+}
+
+
+  render() {
+    return (
+      <TouchableHighlight
+          style={{flex:1}}
+          onPress={() => this.props.appendNumber()}
+          onShowUnderlay={() => this.onPressIn()}
+          onHideUnderlay={() => this.onPressOut()}
+          underlayColor="transparent">
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          <Animated.Text style={{fontSize: this.state.fontSizeAnim, position: 'relative', bottom: this.state.fontPos}}>
+            {this.props.text}
+          </Animated.Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
+
 
 AppRegistry.registerComponent('splitBill', () => splitBill);
 
